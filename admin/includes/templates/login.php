f42fe045251a0f32135f06b759521651e794b9e6
MAshraf <ee.mashraf@gmail.com>
2018-01-05 09:20:05 +0200

    login p1

--- admin/includes/templates/login.php
+++ admin/includes/templates/login.php
@@ -1,4 +1,25 @@
-<form class="login">
+<?php
+  // come from post request
+  if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $username = $_POST['user'];
+    $password = $_POST['password'];
+    $hashedPass = sha1($password);
+    // check user exist
+    $stmt = $db=>prepare("SELECT Username, Password FROM users WHERE Username = ? AND Password = ? AND GroupID = 1");
+    $stmt->execute(array($username, $hashedPass));
+    $count = $stmt->rowCount();
+    // check count
+    if ($count > 0){
+      echo 'Welcome ' . $username;
+    }
+  }
+?>
+
+<form 
+  class="login" 
+  action="<?php echo $_SERVER['PHP_SELF'] ?>" 
+  method="POST"
+>
   <h4 class="text-center">
     Admin Login
   </h4>
