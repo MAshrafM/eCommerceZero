da59999793a5ea76cfb1744e6b22f37a590fcd0f
MAshraf <ee.mashraf@gmail.com>
2018-01-22 17:19:43 +0200

    dashboard layout

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -2,7 +2,63 @@
   $pageTitle = 'Dashboard';
   include 'init.php';
   if(isset($_SESSION['Username'])){
-    echo 'Welcome ' . $_SESSION['Username'];
+  // Dashboard
+?>
+<div class="container home-stats text-center">
+  <h1 class="text-center">Dashboard</h1>
+  <div class="row">
+    <div class="col-md-3">
+      <div class="stat">
+        Total Members
+        <span>200</span>
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="stat">
+        Pending Members
+        <span>200</span>
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="stat">
+        Total Items
+        <span>200</span>
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="stat">
+        Total Comments
+        <span>200</span>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="container latest">
+  <div class="row">
+    <div class="col-sm-6">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <i class="fa fa-users"></i> Latest Registered Users.
+        </div>
+        <div class="panel-body">
+          TO DO 
+        </div>
+      </div>
+    </div>
+    
+    <div class="col-sm-6">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <i class="fa fa-tag"></i> Latest Items.
+        </div>
+        <div class="panel-body">
+          TO DO 
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php
     include $tpl."footer.php";
   } else {
     header('Location: index.php');
