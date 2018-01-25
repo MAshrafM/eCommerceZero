f77c5db01c9f7774e64f7e1e40a13a82362727a2
MAshraf <ee.mashraf@gmail.com>
2018-01-25 14:13:43 +0200

    show cat

--- admin/includes/templates/CategoriesViews/show.php
+++ admin/includes/templates/CategoriesViews/show.php
@@ -1,47 +1,41 @@
 <?php 
-  $query = '';
-  if(isset($_GET['page']) && $_GET['page'] == 'Pending') {
-    $query = 'AND RegStatus = 0';
-  }
   // select all users except admins
-  $stmt = $db->prepare("SELECT * FROM users WHERE GroupID != 1 $query");
+  $stmt = $db->prepare("SELECT * FROM categories");
   $stmt->execute();
   $rows = $stmt->fetchAll();
 ?>
 
-<h1 class="text-center">Manage Members</h1>
+<h1 class="text-center">Manage Categories</h1>
 <div class="container">
   <div class="table-responsive text-center">
     <table class="main-table table table-bordered">
       <tr>
         <th>#ID</th>
-        <th>Username</th>
-        <th>Email</th>
-        <th>Full Name</th>
-        <th>Register Date</th>
+        <th>Category</th>
+        <th>Description</th>
+        <th>Order</th>
+        <th>Visible</th>
+        <th>Comment</th>
+        <th>Ads</th>
         <th>Control</th>
       </tr>
       <?php foreach($rows as $row) { ?>
         <tr>
-          <td><?php echo $row['UserID']; ?></td>
-          <td><?php echo $row['Username']; ?></td>
-          <td><?php echo $row['Email']; ?></td>
-          <td><?php echo $row['FullName']; ?></td>
-          <td><?php echo $row['Date']; ?></td>
+          <td><?php echo $row['ID']; ?></td>
+          <td><?php echo $row['Name']; ?></td>
+          <td><?php echo $row['Description']; ?></td>
+          <td><?php echo $row['Ordering']; ?></td>
+          <td><?php echo $row['Visibility'] ? 'No' : 'Yes'; ?></td>
+          <td><?php echo $row['Allow_Comment'] ? 'No' : 'Yes'; ?></td>
+          <td><?php echo $row['Allow_Ads'] ? 'No' : 'Yes'; ?></td>
           <td>
-          <a href="?v=Edit&uid=<?php echo $row['UserID']; ?>" class="btn btn-success"><i class="fa fa-edit"></i> Edit</a>
-          <a href="?v=Delete&uid=<?php echo $row['UserID']; ?>" class="btn btn-danger confirm"><i class="fa fa-close"></i> Destroy</a>
-          <?php
-            if($row['RegStatus'] == 0) {
-          ?>
-          <a href="?v=Activate&uid=<?php echo $row['UserID']; ?>" class="btn btn-info activate"><i class="fa fa-check"></i> Activate</a>
-          <?php
-            }
-          ?>
+          <a href="?v=Edit&cid=<?php echo $row['ID']; ?>" class="btn btn-success"><i class="fa fa-edit"></i> Edit</a>
+          <a href="?v=Delete&cid=<?php echo $row['ID']; ?>" class="btn btn-danger confirm"><i class="fa fa-close"></i> Destroy</a>
+          
         </td>
         </tr> 
       <?php } ?>
     </table>
   </div>
-  <a href="?v=Add" class="btn btn-primary"><i class="fa fa-plus"></i> New Member</a>
+  <a href="?v=Add" class="btn btn-primary"><i class="fa fa-plus"></i> New Category</a>
 </div>
