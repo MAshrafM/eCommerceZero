713653892b6b4529dddf0d6f84cc7d3822503886
MAshraf <ee.mashraf@gmail.com>
2018-01-25 14:31:22 +0200

    destroy cat

--- admin/includes/templates/CategoriesViews/Destroy.php
+++ admin/includes/templates/CategoriesViews/Destroy.php
@@ -1,18 +1,18 @@
-<h1 class="text-center">Delete Member</h1>
+<h1 class="text-center">Delete Category</h1>
 <div class="container">
 <?php
   // Check user id exists and is numeric
-  $userid = isset($_GET['uid']) && is_numeric($_GET['uid']) ? intval($_GET['uid']) : 0;
-  $count = checkItem('userid', 'users', $userid);
+  $catid = isset($_GET['cid']) && is_numeric($_GET['cid']) ? intval($_GET['cid']) : 0;
+  $count = checkItem('ID', 'categories', $catid);
   // if exists view edit page
   if($count > 0) {
-    $stmt = $db->prepare("DELETE FROM users WHERE UserID = :userid");
-    $stmt->bindParam(":userid", $userid);
+    $stmt = $db->prepare("DELETE FROM categories WHERE ID = :catid");
+    $stmt->bindParam(":catid", $catid);
     $stmt->execute();
-    $Msg = "<div class='alert alert-success'>Member Deleted</div>";
+    $Msg = "<div class='alert alert-success'>Category Deleted</div>";
     redirectLink($Msg, 'back', 4);
   } else { 
-    $errorMsg = "<div class='alert alert-danger'>No Member is found.</div>";
+    $errorMsg = "<div class='alert alert-danger'>No Category is found.</div>";
     redirectLink($errorMsg);
   } 
 ?>
