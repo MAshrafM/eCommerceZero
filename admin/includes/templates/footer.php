70644101a423f2a1b2f605594faec84e1f33ba5a
MAshraf <ee.mashraf@gmail.com>
2018-02-01 20:25:51 +0200

    footer

--- admin/includes/templates/footer.php
+++ admin/includes/templates/footer.php
@@ -1,6 +1,8 @@
 
-    <footer>
-      eCommerce Shop with ElZero. 2018 &copy; MAshraf.
+    <footer class="well text-center">
+      <div class="container">
+        <p>eCommerce Shop with ElZero. 2018 &copy; MAshraf.</p>
+      </div>
     </footer>
     <script src="<?php echo $scripts; ?>jquery.min.js"></script>
     <script src="<?php echo $scripts; ?>jquery-ui.min.js"></script>
