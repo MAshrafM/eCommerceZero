4bdb3e46ccee84c4c0c09c3665f69ce5f0e7b10a
MAshraf <ee.mashraf@gmail.com>
2018-02-17 18:22:21 +0200

    item box

--- categories.php
+++ categories.php
@@ -6,6 +6,7 @@
 ?>
 <div class="container">
   <h1 class="text-center"><?php echo str_replace("-", " ", $_GET['cname']); ?> </h1>
+  <div class="row">
   <?php 
     if(empty($items)){
   ?>
@@ -13,12 +14,21 @@
   <?php 
     } else {
       forEach($items as $item) {
-        echo $item['Name'];
   ?>
-      
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail item-box">
+          <span class="price-tag"><?php echo $item['Price']; ?></span>
+          <img src="./layout/images/holder.png" alt="holder" class="img-responsive"/>
+          <div class="caption">
+            <h3><?php echo $item['Name']; ?></h3>
+            <p><?php echo $item['Description'];?></p>
+          </div>
+        </div>
+      </div>
   <?php
       }
     }
   ?>
+  </div>
 </div>
 <?php include $tpl."footer.php"; ?>
