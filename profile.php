00352316c71e30198d797e926f66ee33c8a98b00
MAshraf <ee.mashraf@gmail.com>
2018-02-22 20:33:19 +0200

    basic structure

--- profile.php
+++ profile.php
@@ -1,6 +1,44 @@
 <?php
-  $pageTitle = 'Home';
+  $pageTitle = 'Profile';
   include "init.php";
 ?>
+<h1 class="text-center"> Profile </h1>
+<div class="info block">
+  <div class="container">
+    <div class="panel panel-primary">
+      <div class="panel-heading">
+        My Information
+      </div>
+      <div class="panel-body">
+      
+      </div>
+    </div>
+  </div>
+</div>
   
+<div class="Ads block">
+  <div class="container">
+    <div class="panel panel-primary">
+      <div class="panel-heading">
+        My Ads
+      </div>
+      <div class="panel-body">
+      
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="Comments block">
+  <div class="container">
+    <div class="panel panel-primary">
+      <div class="panel-heading">
+        My Comments
+      </div>
+      <div class="panel-body">
+      
+      </div>
+    </div>
+  </div>
+</div>
 <?php include $tpl."footer.php"; ?>
