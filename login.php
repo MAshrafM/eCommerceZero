1e07095a0bd490a41fb0a9befca6986a167580a5
MAshraf <ee.mashraf@gmail.com>
2018-03-08 19:55:23 +0200

    login signup page

--- login.php
+++ login.php
@@ -7,21 +7,26 @@
   $pageTitle = 'Login';
   // come from post request
   if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $username = $_POST['user'];
-    $password = $_POST['password'];
-    $hashedPass = sha1($password);
-    // check user exist
-    $stmt = $db->prepare("SELECT UserID, Username, Password FROM users WHERE Username = ? AND Password = ?");
-    $stmt->execute(array($username, $hashedPass));
-    $row = $stmt->fetch();
-    $count = $stmt->rowCount();
-    // check count
-    if ($count > 0){
-      echo "found";
-      $_SESSION['MemberName'] = $username;
-      header('Location: index.php');
-      exit();
+    if(isset($_POST['login'])){
+      $username = $_POST['user'];
+      $password = $_POST['password'];
+      $hashedPass = sha1($password);
+      // check user exist
+      $stmt = $db->prepare("SELECT UserID, Username, Password FROM users WHERE Username = ? AND Password = ?");
+      $stmt->execute(array($username, $hashedPass));
+      $row = $stmt->fetch();
+      $count = $stmt->rowCount();
+      // check count
+      if ($count > 0){
+        echo "found";
+        $_SESSION['MemberName'] = $username;
+        header('Location: index.php');
+        exit();
+      }
+    } elseif (isset($_POST['signup'])) {
+      
     }
+    
   }
   
 ?>
@@ -30,18 +35,18 @@
     <span class="active" data-class="login">Login</span> | <span data-class="signup">Signup</span>
   </h4>
   <!-- Login Form -->
-  <form class="login">
+  <form class="login" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
     <input class="form-control" type="text" name="user" placeholder="<?php echo lang('USERLOGIN'); ?>" autocomplete="off" />
     <input class="form-control" type="password" name="password" placeholder="<?php echo lang('PASSLOGIN'); ?>" autocomplete="new-password" />
-    <input class="btn btn-primary btn-block" type="submit" value="<?php echo lang('LOGIN'); ?>" />
+    <input class="btn btn-primary btn-block" name="login" type="submit" value="<?php echo lang('LOGIN'); ?>" />
   </form>
   <!-- Signup Form -->
-  <form class="signup">
+  <form class="signup" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
     <input class="form-control" type="text" name="user" placeholder="<?php echo lang('USERLOGIN'); ?>" autocomplete="off" />
     <input class="form-control" type="password" name="password" placeholder="<?php echo lang('PASSLOGIN'); ?>" autocomplete="new-password" />
     <input class="form-control" type="password" name="password2" placeholder="Repeat <?php echo lang('PASSLOGIN'); ?>" autocomplete="new-password" />
     <input class="form-control" type="email" name="email" placeholder="<?php echo lang('EMAIL'); ?>" autocomplete="new-password" />
-    <input class="btn btn-success btn-block" type="submit" value="Signup" />
+    <input class="btn btn-success btn-block" name="signup" type="submit" value="Signup" />
   </form> 
   
 </div>
