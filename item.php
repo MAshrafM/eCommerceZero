c9cfe40593fd396863a2d8846930237ba530d0fc
MAshraf <ee.mashraf@gmail.com>
2018-04-04 22:02:13 +0200

    error handling when no item id

--- item.php
+++ item.php
@@ -4,10 +4,17 @@
   $itemID = isset($_GET['tid']) && is_numeric($_GET['tid']) ? intval($_GET['tid']) : 0;
   $stmt = $db->prepare("SELECT * FROM items WHERE Item_ID = ?");
   $stmt->execute(array($itemID));
-  $item = $stmt->fetch();
+  $count = $stmt->rowCount();
+  if($count > 0){
+    $item = $stmt->fetch();
   
 ?>
 <h1 class="text-center"> <?php echo $item['Name']; ?> </h1>
 
-
+<?php
+  } else {
+    $Msg = "<p class='alert alert-danger'>There is Such ID for this item</p>"
+    redirectLink($Msg)
+  }
+?>
 <?php include $tpl."footer.php"; ?>
