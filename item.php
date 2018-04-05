79207776b73f78d0c571eaeb361f2e6567673ef6
MAshraf <ee.mashraf@gmail.com>
2018-04-05 15:30:43 +0200

    item info

--- item.php
+++ item.php
@@ -10,7 +10,20 @@
   
 ?>
 <h1 class="text-center"> <?php echo $item['Name']; ?> </h1>
-
+<div class="container">
+  <div class="row">
+    <div class="col-md-3">
+      <img src="./layout/images/holder.png" alt="holder" class="img-responsive img-thumbnail center-block"/>
+    </div>
+    <div class="col-md-9">
+      <h2><?php echo $item['Name']; ?></h2>
+      <p><?php echo $item['Description']; ?></p>
+      <span><?php echo $item['Add_Date']; ?></span>
+      <div>Price: $<?php echo $item['Price']; ?></div>
+      <div>Made In: <?php echo $item['Country_Made']; ?></div>
+    </div>
+  </div>
+</div>
 <?php
   } else {
     $Msg = "<p class='alert alert-danger'>There is Such ID for this item</p>"
